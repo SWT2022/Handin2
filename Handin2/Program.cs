7fe0fab619cad43c61a1b814ea0157e5bf30b048
ChynaHoberg <jacobhoberg1234@gmail.com>
2022-03-10 12:28:58 +0100

    Add project files.

---
+++ Handin2/Program.cs
@@ -0,0 +1,44 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+				// Assemble your system here from all the classes
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        door.OnDoorClose();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
+        }
+    }
+}
---
+++ Handin2/StationControl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+
+namespace Ladeskab
+{
+    public class StationControl
+    {
+        // Enum med tilstande ("states") svarende til tilstandsdiagrammet for klassen
+        private enum LadeskabState
+        {
+            Available,
+            Locked,
+            DoorOpen
+        };
+
+        // Her mangler flere member variable
+        private LadeskabState _state;
+        private IChargeControl _charger;
+        private int _oldId;
+        private IDoor _door;
+
+        private string logFile = "logfile.txt"; // Navnet på systemets log-fil
+
+        // Her mangler constructor
+
+        // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
+        private void RfidDetected(int id)
+        {
+            switch (_state)
+            {
+                case LadeskabState.Available:
+                    // Check for ladeforbindelse
+                    if (_charger.Connected)
+                    {
+                        _door.LockDoor();
+                        _charger.StartCharge();
+                        _oldId = id;
+                        using (var writer = File.AppendText(logFile))
+                        {
+                            writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+                        }
+
+                        Console.WriteLine("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.");
+                        _state = LadeskabState.Locked;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+                    }
+
+                    break;
+
+                case LadeskabState.DoorOpen:
+                    // Ignore
+                    break;
+
+                case LadeskabState.Locked:
+                    // Check for correct ID
+                    if (id == _oldId)
+                    {
+                        _charger.StopCharge();
+                        _door.UnlockDoor();
+                        using (var writer = File.AppendText(logFile))
+                        {
+                            writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", id);
+                        }
+
+                        Console.WriteLine("Tag din telefon ud af skabet og luk døren");
+                        _state = LadeskabState.Available;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Forkert RFID tag");
+                    }
+
+                    break;
+            }
+        }
+
+        // Her mangler de andre trigger handlere
+    }
+}
