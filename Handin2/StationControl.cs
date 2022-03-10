7249de384d3f99e35f1b8cae7f2479095222e256
ChynaHoberg <jacobhoberg1234@gmail.com>
2022-03-10 13:04:41 +0100

    Fixed typo

--- Handin2/StationControl.cs
+++ Handin2/StationControl.cs
@@ -4,9 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ladeskab.Interfaces;
+using Handin2.Interfaces;
 
-namespace Ladeskab
+namespace Handin2
 {
     public class StationControl
     {
