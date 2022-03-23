e4dd9379c1d2c8e859f1e0835a2ee750707c348a
Rasmus AA <rasmus-baunvig@live.dk>
2022-03-23 10:23:21 +0100

    StationControl impl.

--- Handin2/StationControl.cs
+++ Handin2/StationControl.cs
@@ -8,9 +8,10 @@ using System.Threading.Tasks;
 
 namespace Handin2
 {
+
     public class StationControl : IStationControl
     {
-        // Enum med tilstande ("states") svarende til tilstandsdiagrammet for klassen
+        //Enum med tilstande("states") svarende til tilstandsdiagrammet for klassen
         private enum LadeskabState
         {
             Available,
@@ -18,8 +19,7 @@ namespace Handin2
             DoorOpen
         };
 
-        // Her mangler flere member variable
-        private LadeskabState _state;
+        // Member variables
         private IChargeControl _charger;
         private bool _doorState;
         private int _oldId;
@@ -27,10 +27,10 @@ namespace Handin2
         private IDoor _door;
         private IRfidReader _reader;
         private IDisplay _display;
+        private LadeskabState _state;
 
         private string logFile = "logfile.txt"; // Navnet på systemets log-fil
 
-        // Her mangler constructor
         public StationControl(IChargeControl charger, IDoor door, IRfidReader reader, IDisplay display)
         {
             _display = display;
@@ -43,24 +43,6 @@ namespace Handin2
 
         }
 
-        private void HandleDoorStateEvent(object s, DoorStateEventArgs e)
-        {
-            _doorState = e.DoorState;
-            if (_doorState == true)
-            {
-                DoorOpened();
-            }
-            else
-                DoorClosed();
-        }
-
-        private void HandleRfidReaderEvent(object s, RfidReaderEventArgs e)
-        {
-            _newId = e.Id;
-            RfidDetected(_newId);
-        }
-
-        // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
         public void RfidDetected(int id)
         {
             switch (_state)
@@ -114,16 +96,156 @@ namespace Handin2
                     break;
             }
         }
-
         public void DoorOpened()
         {
+            Console.WriteLine("Door Opened");
             _display.DisplayConnectPhone();
         }
         public void DoorClosed()
         {
+            Console.WriteLine("Door Closed");
             _display.DisplayReadRfid();
         }
 
-        // Her mangler de andre trigger handlere
+        public bool CheckId(int id, int oldId)
+        {
+            return true;
+        }
+
+        //Handle Events
+        private void HandleDoorStateEvent(object s, DoorStateEventArgs e)
+        {
+            _doorState = e.DoorState;
+            if (_doorState == true)
+            {
+                DoorOpened();
+            }
+            else
+                DoorClosed();
+        }
+
+        private void HandleRfidReaderEvent(object s, RfidReaderEventArgs e)
+        {
+            _newId = e.Id;
+            RfidDetected(_newId);
+        }
+
+
+        //    // Enum med tilstande ("states") svarende til tilstandsdiagrammet for klassen
+        //    private enum LadeskabState
+        //    {
+        //        Available,
+        //        Locked,
+        //        DoorOpen
+        //    };
+
+        //    // Her mangler flere member variable
+        //    private LadeskabState _state;
+        //    private IChargeControl _charger;
+        //    private bool _doorState;
+        //    private int _oldId;
+        //    private int _newId;
+        //    private IDoor _door;
+        //    private IRfidReader _reader;
+        //    private IDisplay _display;
+
+        //    private string logFile = "logfile.txt"; // Navnet på systemets log-fil
+
+        //    // Her mangler constructor
+        //    public StationControl(IChargeControl charger, IDoor door, IRfidReader reader, IDisplay display)
+        //    {
+        //        _display = display;
+        //        _charger = charger;
+        //        _door = door;
+        //        _reader = reader;
+        //        _door.DoorStateEvent += HandleDoorStateEvent;
+        //        _reader.RfidReaderEvent += HandleRfidReaderEvent;
+        //        _state = LadeskabState.Available;
+
+        //    }
+
+        //    private void HandleDoorStateEvent(object s, DoorStateEventArgs e)
+        //    {
+        //        _doorState = e.DoorState;
+        //        if (_doorState == true)
+        //        {
+        //            DoorOpened();
+        //        }
+        //        else
+        //            DoorClosed();
+        //    }
+
+        //    private void HandleRfidReaderEvent(object s, RfidReaderEventArgs e)
+        //    {
+        //        _newId = e.Id;
+        //        RfidDetected(_newId);
+        //    }
+
+        //    // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
+        //    public void RfidDetected(int id)
+        //    {
+        //        switch (_state)
+        //        {
+        //            case LadeskabState.Available:
+        //                // Check for ladeforbindelse
+        //                if (_charger.isConnected())
+        //                {
+        //                    _door.LockDoor();
+        //                    _charger.StartCharge();
+        //                    _oldId = id;
+        //                    using (var writer = File.AppendText(logFile))
+        //                    {
+        //                        writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+        //                    }
+        //                    _display.DisplayCharging();
+        //                    //Console.WriteLine("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.");
+        //                    _state = LadeskabState.Locked;
+        //                }
+        //                else
+        //                {
+        //                    _display.DisplayConnectError();
+        //                    //Console.WriteLine("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+        //                }
+
+        //                break;
+
+        //            case LadeskabState.DoorOpen:
+        //                // Ignore
+        //                break;
+
+        //            case LadeskabState.Locked:
+        //                // Check for correct ID
+        //                if (id == _oldId)
+        //                {
+        //                    _charger.StopCharge();
+        //                    _door.UnlockDoor();
+        //                    using (var writer = File.AppendText(logFile))
+        //                    {
+        //                        writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", id);
+        //                    }
+
+        //                    Console.WriteLine("Tag din telefon ud af skabet og luk døren");
+        //                    _state = LadeskabState.Available;
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine("Forkert RFID tag");
+        //                }
+
+        //                break;
+        //        }
+        //    }
+
+        //    public void DoorOpened()
+        //    {
+        //        _display.DisplayConnectPhone();
+        //    }
+        //    public void DoorClosed()
+        //    {
+        //        _display.DisplayReadRfid();
+        //    }
+
+        //    // Her mangler de andre trigger handlere
+        //}
     }
 }
---
+++ Handin2_test/UnitTestStationControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handin2;
+using NSubstitute;
+using NUnit.Framework;
+using UsbSimulator;
+using System.IO;
+
+namespace Handin2_test
+{
+    public class UnitTestStationControl
+    {
+        private StationControl uut;
+        private IDisplay _display;
+        private IChargeControl _charger;
+        private IDoor _door;
+        private IRfidReader _reader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _display = Substitute.For<IDisplay>();
+            _reader = Substitute.For<IRfidReader>();
+            _charger = Substitute.For<IChargeControl>();
+            _door = Substitute.For<IDoor>();    
+
+            uut = new StationControl( _charger, _door, _reader, _display );
+        }
+
+        [Test]
+        public void DoorOpened_returnsCorrect()
+        {
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                uut.DoorOpened();
+
+                string expected = string.Format("Door Opened{0}", Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+
+
+        }
+
+    }
+
+
+}
+
